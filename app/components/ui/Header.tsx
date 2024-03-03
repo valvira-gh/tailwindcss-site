2d0acba594af4cc7fb460b06d9ecaefbc5ae4e8b
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-03-03 17:42:43 +0200

    tsconfig.json

---
+++ app/components/ui/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
+    </header>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const HomePage: React.FC = () => {
   return (
     <main className="w-screen h-screen flex flex-col align-center items-center">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
+      <div>Home</div>
     </main>
   );
 };
